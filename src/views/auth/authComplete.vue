f49e406fb192e0857f765c2be0a8fa150529a1df
aosdinkim <master@awesomble.com>
2025-03-19 17:38:53 +0900

    Dev init

--- src/views/auth/authComplete.vue
+++ src/views/auth/authComplete.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import { nextTick, onMounted } from 'vue'
+import { onMounted } from 'vue'
 const router = useRouter()
 
 onMounted(async () => {
-  await nextTick()
-  const redirectPath = localStorage.getItem('redirectPath')
-  if (redirectPath) {
-    await router.push(redirectPath)
-    console.log(redirectPath)
-    localStorage.removeItem('redirectPath')
-  } else await router.push({name: 'main-home'})
+  const redirectPath = localStorage.getItem('redirectPath') || '/'
+  localStorage.removeItem('redirectPath')
+  await router.push(redirectPath)
 })
+
 </script>
