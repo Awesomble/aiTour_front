b7280fde905025139991dd05335af63488eda67d
aosdinkim <master@awesomble.com>
2025-03-19 17:34:16 +0900

    Dev init

--- src/views/auth/Signin.vue
+++ src/views/auth/Signin.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import { signInWithRedirect, getCurrentUser, signOut, fetchAuthSession, fetchUserAttributes } from 'aws-amplify/auth'
+import { useRoute, useRouter } from 'vue-router'
+import { signInWithRedirect } from 'aws-amplify/auth'
+
 const route = useRoute()
+const router = useRouter()
 
-
-const signInWithRedirectCall = async (redirectPath = '/') => {
+const signInWithRedirectCall = async () => {
   try {
     localStorage.setItem('redirectPath', String(route.query?.redirect))
     await signInWithRedirect().catch(() => null)
@@ -13,19 +14,19 @@ const signInWithRedirectCall = async (redirectPath = '/') => {
     console.error(err)
   }
 }
+
+// 로그인 후 돌아올 때 쿼리의 redirect 값이 있으면 해당 페이지로 이동
+onMounted(() => {
+  const redirect = (route.query.redirect as string) || '/'
+  router.push(redirect)
+})
 </script>
 
 <template>
   <v-container fluid>
-    <!-- 기존 PlaceDetail 관련 템플릿 -->
-
     <!-- 인증 관련 버튼 추가 -->
     <div class="auth-buttons">
-      <v-btn color="primary" @click="signInWithRedirectCall"> Sign In </v-btn>
-<!--      <v-btn color="error" @click="signOutRedirect()"> Sign Out </v-btn>-->
+      <v-btn color="primary" @click="signInWithRedirectCall">Sign In</v-btn>
     </div>
-    <!-- 기존 템플릿 내용 계속 -->
   </v-container>
 </template>
-
-<style scoped lang="scss"></style>
